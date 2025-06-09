39c14f9128a63da8395f759d559e43538603eaeb
hiio420official <hiio420.official@gmail.com>
2025-06-09 13:05:20 +0900

    day5

--- nestjs/day2/src/cats/cats.controller.ts
+++ nestjs/day2/src/cats/cats.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
-
+// cats.controller.ts
+import { HttpStatus, HttpException } from "@nestjs/common";
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { ForbiddenException } from '../common/exception/forbidden.exception';
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    constructor(private readonly catsService: CatsService) { }
+
+    @Get()
+    async findAll() {
+        throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
+
+
+    @Get()
+    async findAll() {
+        try {
+            await this.service.findAll()
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.FORBIDDEN,
+                error: 'This is a custom message',
+            }, HttpStatus.FORBIDDEN, {
+                cause: error
+            });
+        }
+    }
+
+
+    @Get()
+    async findAll() {
+        throw new ForbiddenException();
+    }
+
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        const cat = await this.catsService.findById(id);
+        if (!cat) {
+            throw new NotFoundException(`고양이 ID ${id}를 찾을 수 없습니다.`);
+        }
+        return cat;
+    }
+}
